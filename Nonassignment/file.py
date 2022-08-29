e1149efdeafeee891219d9da86a2cbf35a650458
Cole Winchester <cowinch@gmail.com>
2022-08-28 21:13:08 -0500

    intro

---
+++ Nonassignment/file.py
@@ -0,0 +1,15 @@
+# import random
+# print(random.randint(2,5))
+
+#numbers in python are not decimal friendly by default. they must be written as floats or ints
+num =25
+dec =4.2
+int_to_float = float(35)
+float_to_int = int(44.2)
+int_to_complex = complex(35)
+print(int_to_float)
+print(float_to_int)
+print(int_to_complex)
+print(type(int_to_float))
+print(type(float_to_int))
+print(type(int_to_complex))
