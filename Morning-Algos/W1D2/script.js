bfd4f12318041f30a75f6f0e60b8c61a1cfc49ec
Cole Winchester <cowinch@gmail.com>
2022-08-30 12:21:39 -0500

    algos

---
+++ Morning-Algos/W1D2/script.js
@@ -0,0 +1,33 @@
+/* 
+    String: Reverse
+    Given a string,
+    return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "asdgjksdhfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgshfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgshfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgahfgkjsdhfgkjsdhfgklsdhfgkjsdhgkjsdhfgkjsenrtyjskldfgjhskdjnvsdklghrkyhsdkfnjvsdkfjhgkstgstnljvsfsldifhaiuoerghsdklfhgatnljvsfsldifhaiuoerghsdklfhgaklgf";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString (str) {
+    return str.split("").reverse().join("")
+}
+
+//TEST CODE FOR REVERSE
+console.log(reverseString(str1))
+console.log(reverseString(str2))
+console.log(reverseString(str3))
+console.log(reverseString(str4))
