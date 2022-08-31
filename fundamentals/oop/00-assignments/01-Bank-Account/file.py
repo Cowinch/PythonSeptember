a6209f6573f095013ecd2209c1d69217492e0c00
Cole Winchester <cowinch@gmail.com>
2022-08-30 19:29:23 -0500

    Bank Account complete

--- fundamentals/oop/00-assignments/01-Bank-Account/file.py
+++ fundamentals/oop/00-assignments/01-Bank-Account/file.py
@@ -1,40 +1,70 @@
+from random import randint
 
-user=[
-    {
-        'first_name': 'Cole',
-        'last_name': 'Winchester',
-        'email': 'AnEmail@email.com',
-        'age': '26'
-    },
-    {
-        'first_name': 'Desmond',
-        'last_name': 'Miles',
-        'email': 'TheEmail@gmail.com',
-        'age':'30'
-    }
-]
 
-class User:
-    def __init__(self, user_data):
-        self.first_name=user_data['first_name']
-        self.last_name=user_data['last_name']
-        self.email=user_data['email']
-        self.age=user_data['age']
-        self.is_rewards_member=False
-        self.gold_card_points=0
+class BankAccount:
+    all_accounts=[]
+    overdraft_fee=5
+    def __init__(self, int_rate, balance):
+        self.int_rate=int_rate
+        self.balance=balance
+        BankAccount.all_accounts.append(self)
 
-    def display_info(self):
-        print(f"First name: {self.first_name}")
-        print(f"Last name: {self.last_name}")
-        print(f"Email: {self.email}")
-        print(f"Age: {self.age}")
+    def deposit(self, amount):
+        self.balance=self.balance+amount
+        print('===================')
+        print(f'New balance after a deposit of ${amount}: ${self.balance}')
+        print('===================')
         return self
 
-    def enroll(self):
-        pass
+    def withdraw(self, amount):
+        if self.balance>=amount:
+            self.balance=self.balance-amount
+            print('===================')
+            print(f"Cash Remaining after a withdraw of ${amount}: ${self.balance}")
+            print('===================')
+            print('')
+        else:
+            print('===================')
+            print(f"Insufficient funds, charging a ${BankAccount.overdraft_fee} fee")
+            self.balance=self.balance-BankAccount.overdraft_fee
+            print(f'New total after overdraft fee: ${self.balance}')
+            print('===================')
+            print('')
+        return self
 
-    def spend_points(self,amount):
-        pass
+    def display_account_info(self):
+        print('===================')
+        print("Account Info:")
+        print(f'Balance: ${self.balance}')
+        print(f'Interest rate: {self.int_rate*100}%')
+        print('===================')
+        print('')
+        return self
 
-User1=User(user[0])
-User1.display_info()
+    def yield_interest(self):
+        if self.balance>0:
+            self.balance=self.balance+(self.balance*self.int_rate)
+            print('===================')
+            print(f'New remaining balance after interest of {self.int_rate*100}%: {self.balance}')
+            print('===================')
+            print('')
+        else:
+            print('===================')
+            print('Unfortately as your funds are too low, you did not receive an increase from interest')
+            print('===================')
+            print('')
+        return self
+
+    @classmethod 
+    def print_all_bank_accounts(cls):
+        for account in cls.all_accounts:
+            account.display_account_info()
+
+user1=BankAccount(.01,200)
+user2=BankAccount(.025,400)
+
+user1.display_account_info().deposit(randint(1,800)).deposit(12).deposit(150).withdraw(randint(1,1200)).yield_interest().display_account_info()
+
+user2.display_account_info().deposit(randint(1,700)).deposit(55).withdraw(randint(1,750)).withdraw(randint(1,500)).withdraw(randint(1,250)).withdraw(129).yield_interest().display_account_info()
+
+BankAccount.print_all_bank_accounts()
