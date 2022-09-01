2b8de47b652e9bf42a6b7969814773298ec9e5dd
Cole Winchester <cowinch@gmail.com>
2022-09-01 18:43:56 -0500

    playground complete

--- flask/fundamentals/00-assignments/01-Playground/server.py
+++ flask/fundamentals/00-assignments/01-Playground/server.py
@@ -4,5 +4,18 @@ app=Flask(__name__)
 @app.route('/')
 def nothing_here():
     return "Theres nothing here"
+
+@app.route('/play')
+def play():
+    return render_template('play.html', number=3, color='aqua')
+
+@app.route('/play/<int:number>')
+def playAmount(number):
+    return render_template('play.html', number=number, color='aqua')
+
+@app.route('/play/<int:number>/<color>')
+def playAmountColor(number, color):
+    return render_template('play.html', number=number, color=color)
+
 if __name__=='__main__':
     app.run(debug=True)
