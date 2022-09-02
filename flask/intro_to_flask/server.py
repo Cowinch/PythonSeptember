c7431a512735f364bb271678fc3d4f575659e761
Cole Winchester <cowinch@gmail.com>
2022-09-02 12:47:21 -0500

    friday am lecture

--- flask/intro_to_flask/server.py
+++ flask/intro_to_flask/server.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app=Flask(__name__)
+app.secret_key = 'no secrets on github'
 
 
+
+#everything up here is pertaining to day 1 flask
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -40,6 +43,36 @@ def template():
     new_variable='This is a test tsring'
     return render_template("index.html", new_variable_html=new_variable)
     #the variable on the left is what we call it on html, the value on the right must match the variable defined above
+
+
+
+
+#everything down here is pertaining to day 2 flask
+@app.route('/input')
+def input():
+    return render_template('input.html')
+@app.route('/process_form', methods=['POST']) #methos list for specifying methos to listen for
+def process_form():
+    print(request.form)
+    session['dog_name'] = request.form['dog_name']
+    session["dog_breed"] = request.form['dog_breed']
+    session['form_num'] = request.form['form_num']
+    # return render_template('display.html')#typically we WONT render on "ACTION ROUTES"
+    return redirect('/show_info')
+
+@app.route('/show_info')
+def show_info():
+    if 'form_num' in session:
+        num=session['form_num']
+    name = session['dog_name']
+    breed = session['dog_breed']
+    return render_template("display.html", name=name,breed=breed, num=num)
+
+@app.route('/clear_session')
+def clear_session():
+    session.clear()#would remove all keys
+    # del session['dog_breed']#would remove one key
+    return redirect('/input')
 
 if __name__=='__main__':
     app.run(debug=True)
