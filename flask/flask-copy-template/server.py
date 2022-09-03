de675ca3254d017f0c38973311e9e1983194a1b0
Cole Winchester <cowinch@gmail.com>
2022-09-03 13:34:15 -0500

    template updated

--- flask/flask-copy-template/server.py
+++ flask/flask-copy-template/server.py
@@ -6,5 +6,10 @@ app.secret_key = 'no secrets on github'
 def root():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e): #as far as I can tell this parameter literally exists just to stop a positional argument error.
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 if __name__=='__main__':
     app.run(debug=True)
--- flask/fundamentals/00-assignments/04-Dojo-Survey/server.py
+++ flask/fundamentals/00-assignments/04-Dojo-Survey/server.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, make_response, url_for
 app=Flask(__name__)
+app.secret_key = 'no secrets on github'
 
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e): #as far as I can tell this parameter literally exists just to stop a positional argument error.
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__=='__main__':
     app.run(debug=True)
