172324e386f1e45e8c335207653e1d5928bca8ca
Cole Winchester <cowinch@gmail.com>
2022-09-03 01:27:40 -0500

    notes on python shell added

--- flask/fundamentals/00-assignments/03-HTML-Counter/server.py
+++ flask/fundamentals/00-assignments/03-HTML-Counter/server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, session
 app=Flask(__name__)
 app.secret_key = 'no secrets on github'
 
+#if we want to view the values stored in the cookies, we can open a python shell (thats gunna be a terminal with >>>, just type python into cmder). Then well type import base64 and then base64.urlsafe_b64decode('') insert the first part of the cookies value (everything before the first . ) followed by === all inside of the ''
+
 @app.route('/')
 def root():
     #this if statement is responsible for generating the counter in session. We dont want session being set to 0 each time the site is loaded, so we use an if statement to check if it exists or not.
