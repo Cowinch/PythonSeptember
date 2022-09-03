16547c598200f672c1e4b199e6afecde9f8bf010
Cole Winchester <cowinch@gmail.com>
2022-09-03 02:46:24 -0500

    404 page added

--- flask/fundamentals/00-assignments/03-HTML-Counter/server.py
+++ flask/fundamentals/00-assignments/03-HTML-Counter/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, make_response, url_for
 app=Flask(__name__)
 app.secret_key = 'no secrets on github'
 
@@ -23,6 +23,11 @@ def reset():
     #session.clear() is used to clear our backpack, AKA session
     session.clear()
     return redirect('/')
+
+@app.errorhandler(404)
+def page_not_found(e): #as far as I can tell this parameter literally exists just to stop a positional argument error.
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__=='__main__':
     app.run(debug=True)
