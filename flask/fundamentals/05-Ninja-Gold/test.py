1b09edfe11c824ab240f3514ff8f52c77e0481a4
Cole Winchester <cowinch@gmail.com>
2022-09-06 14:30:01 -0500

    note update

--- flask/fundamentals/05-Ninja-Gold/test.py
+++ flask/fundamentals/05-Ninja-Gold/test.py
@@ -1,3 +1,4 @@
+#this file was made just to test python notation in for loops
 test=['Earned 4 gold from the farm!', 'Earned 6 gold from the cave!', 'Earned 4 gold from the cave!', 'Earned 0 gold from the farm!', 'Earned 6 gold from the farm!']
 
 for i in test:
