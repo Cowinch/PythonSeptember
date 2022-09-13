8f4387ef266b716caa949e9d499829597ee4ed1d
Cole Winchester <cowinch@gmail.com>
2022-09-13 16:34:09 -0500

    javascrip added

---
+++ flask/users-template/flask_app/static/js/script.js
@@ -0,0 +1,8 @@
+//thise code is used to ask the user if they're sure they want to delete something
+var elems = document.getElementsByClassName('confirmation');
+    var confirmIt = function (e) {//this code comes from https://stackoverflow.com/questions/10462839/how-to-display-a-confirmation-dialog-when-clicking-an-a-link
+        if (!confirm('Are you sure about that?')) e.preventDefault();
+    };
+    for (var i = 0, l = elems.length; i < l; i++) {
+        elems[i].addEventListener('click', confirmIt, false);
+    }
